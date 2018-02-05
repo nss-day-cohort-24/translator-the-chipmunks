b0ce93bbc031e17043df0aff6654902b6508f625
Dan Watson <danwatson00@yahoo.com>
2018-02-05 15:17:13 -0600

    second commit

---
+++ js/spanish.js
@@ -0,0 +1,12 @@
+var translateText = (function (origTranslateText) {
+    var spanishWords = { "merry": "feliz", "christmas": "Navidad", "and": "y", "happy": "feliz", "new": "nuevo", "year": "año", "birthday": "cumpleaños", "Jesus": "Jesús", "God": "Dios", "you": "ti", "to you": "a ti", "have": "tienes", "we": "nosotoros", "wish": "deseo"};
+
+
+    origTranslateText.translateToSpanish = function (englishWord) {
+        return spanishWords[englishWord];
+    };
+
+    return origTranslateText;
+
+
+})(translateText);
