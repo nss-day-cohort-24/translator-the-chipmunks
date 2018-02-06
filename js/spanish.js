f476e5bf84c8050a572cbb76ad8a1f5b312011ba
Dan Watson <danwatson00@yahoo.com>
2018-02-06 11:07:37 -0600

    more spanish words

--- js/spanish.js
+++ js/spanish.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var translateText = (function (origTranslateText) {
-    var spanishWords = { "merry": "feliz", "christmas": "Navidad", "and": "y", "happy": "feliz", "new": "nuevo", "year": "año", "birthday": "cumpleaños", "Jesus": "Jesús", "God": "Dios", "you": "ti", "to you": "a ti", "have": "tienes", "we": "nosotoros", "wish": "deseo"};
+    var spanishWords = { "merry": "feliz", "christmas": "Navidad", "and": "y", "happy": "feliz", "new": "nuevo", "year": "año", "birthday": "cumpleaños", "Jesus": "Jesús", "God": "Dios", "you": "ti", "to": "a", "have": "tienes", "we": "nosotoros", "wish": "deseo", "baby": "niño"};
 
 
     origTranslateText.translateToSpanish = function (englishWord) {
