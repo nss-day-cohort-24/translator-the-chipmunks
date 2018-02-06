16354b6b618831ee05d64ee4e47653c19ed0e758
Anastasiia Somchyk <e4nastya@gmail.com>
2018-02-06 09:42:02 -0600

    first commit

---
+++ js/russian.js
@@ -0,0 +1,21 @@
+// console.log("hi")
+var translateText = (function(russian) {
+    var russianWords = {"merry": "весёлого",
+                      "christmas": "рождества", 
+                      "and": "и", 
+                      "happy": "счастливого", 
+                      "new": "нового", 
+                      "year": "года" };
+              
+
+
+       russian.translateToRussian = function (input) {
+        return russianWords[input] 
+       };
+	
+
+	return russian;
+
+
+})
+console.log(translateText);
